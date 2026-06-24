7cc30416656f37a0d1d9ddc1f68776b53f141fa9
ankit <ankitkumargre1904@gmail.com>
2026-06-24 19:48:27 +0530

    static method

---
+++ Calculator static method exmaple 2.py
@@ -0,0 +1,11 @@
+class Calculator:
+    @staticmethod
+    def add(a,b):
+        print(a+b)
+
+    @staticmethod
+    def multiply(a,b):
+        print(a*b)
+
+Calculator.add(2,3)
+Calculator.multiply(2,4)
---
+++ static Even or Odd.py
@@ -0,0 +1,7 @@
+class Number:
+    @staticmethod
+    def is_even(num):
+        print("True")
+
+Number.is_even(10)
+Number.is_even(7)
