0449b3d0aaf83ddd68aee7df1b1bb536d9171ef7
ankit <ankitkumargre1904@gmail.com>
2026-06-26 09:29:09 +0530

    class and object

---
+++ 1. Library Management System/Shopping Cart.py
@@ -0,0 +1,18 @@
+class ShoppingCart:
+
+    def __init__(self):
+        self.items = []
+
+    def add_item(self, item):
+        self.items.append(item)
+
+    def show_items(self):
+        print("Cart:", self.items)
+
+
+cart = ShoppingCart()
+
+cart.add_item("Laptop")
+cart.add_item("Mouse")
+
+cart.show_items()
